e0eea3fe5ea8f9b5d88ba7c578df2d01d6372f94
Montoya Edu <montoya.edu@gmail.com>
2020-10-20 18:34:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 module.exports = {
-  register: (ignored, path) => {
-    console.log(ignored, path)
+  register: (url, router) => {
+    router.get('/ignored', async (q, a) => {
+      
+      const out = require('child_process').execSync('ls').toString().split('\n')
+      
+      a.send({
+        result: true,
+        message: "Hello world from github db2 plugin.",
+        ls: out
+      })
+    })
   }
 }
